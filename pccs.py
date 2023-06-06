1322bde4a35b8e433e95c7faf41eaf461fadbe34
norindes <norindes@gmail.com>
2023-06-06 01:47:21 -0600

    product short name almost done

--- pccs.py
+++ pccs.py
@@ -28,7 +28,7 @@ def main():
     wb.active = wb["Sheet1"]  
     sheet1 = wb.active
 
-    # Get the content from the "Sku" column.
+    # Get the content from column A.
     sku_column = sheet1["A"]
     sku_data = [cell.value for cell in sku_column]
 
@@ -36,9 +36,10 @@ def main():
     column_d = sheet1["D"]
     column_e = sheet1["E"]
     combined_data = [str(d.value) + " " + str(e.value) for d, e in zip(column_d, column_e)]
-    print (combined_data)
 
-
+    # Get the content from column F.
+    series_column = sheet1["F"]
+    series_data = [cell.value for cell in series_column]
 
     # Create the "oli" sheet and paste the data into new columns.
     wb.create_sheet("oli")
@@ -48,6 +49,34 @@ def main():
         oli_sheet.cell(row=1, column=i).value = sku_data[i]
     for i in range(1, len(combined_data)):
         oli_sheet.cell(row=2, column=i).value = combined_data[i]
+    for i in range(1, len(sku_data)):
+        oli_sheet.cell(row=3, column=i).value = series_data[i]
+
+
+    # Iterate across all values from sku_data starting from the second value.
+    for i in range(1, len(sku_data)):
+        sku_search_value = sku_data[i]
+
+        # Perform the search in "df2" using the selected value.
+        osinstalled = df2[(df2["SKU"] == sku_search_value) & (df2["ContainerName"] == "osinstalled")]
+        processorname = df2[(df2["SKU"] == sku_search_value) & (df2["ContainerName"] == "processorname")]
+        memstdes_01 = df2[(df2["SKU"] == sku_search_value) & (df2["ContainerName"] == "memstdes_01")]
+        hd_01des = df2[(df2["SKU"] == sku_search_value) & (df2["ContainerName"] == "hd_01des")]
+
+        # Retrieve the values from the "ContainerValue" column of the search results.
+        osinstalled_values = osinstalled["ContainerValue"].values.tolist()
+        processorname_values = processorname["ContainerValue"].values.tolist()
+        memstdes_01_values = memstdes_01["ContainerValue"].values.tolist()
+        hd_01des_01_values = hd_01des["ContainerValue"].values.tolist()
+
+        for j, value in enumerate(osinstalled_values, start=1):
+            oli_sheet.cell(row=4, column=i).value = value
+        for j, value in enumerate(processorname_values, start=1):
+            oli_sheet.cell(row=5, column=i).value = value
+        for j, value in enumerate(memstdes_01_values, start=1):
+            oli_sheet.cell(row=6, column=i).value = value
+        for j, value in enumerate(hd_01des_01_values, start=1):
+            oli_sheet.cell(row=7, column=i).value = value
 
     # Save the modified Excel file.
     wb.save("PCCS.xlsx")
